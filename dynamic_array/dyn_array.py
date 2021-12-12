ad760efc348c0e744e6163b7c92ef3414fbbbf1a
Alexander Bondarenko <sascha.bondarenko@gmail.com>
2021-12-12 19:54:19 +0100

    adt dynarray, realization

--- dynamic_array/dyn_array.py
+++ dynamic_array/dyn_array.py
@@ -1,3 +1,4 @@
+import ctypes
 from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
 
@@ -5,7 +6,6 @@ T = TypeVar('T')
 
 
 class ADynArray(ABC, Generic[T]):
-
     INSERT_NIL = 0
     INSERT_OK = 1
     INSERT_ERR = 2
@@ -69,7 +69,7 @@ class ADynArray(ABC, Generic[T]):
 
     # заполненность буфера в интервале [0, 1]
     def filling_degree(self) -> float:
-        return self.count()/self.capacity()
+        return self.count() / self.capacity()
 
     @abstractmethod
     def remove_status(self) -> int:
@@ -78,3 +78,82 @@ class ADynArray(ABC, Generic[T]):
     @abstractmethod
     def insert_status(self) -> int:
         pass
+
+
+class DynArray(ADynArray):
+
+    def __init__(self):
+        self._count = 0
+        self._capacity = 16
+        self.default_capacity = 16
+        self.array = self._make_array(self._capacity)
+        self._insert_status = self.INSERT_NIL
+        self._remove_status = self.REMOVE_NIL
+        self._get_status = self.GET_NIL
+
+    def _make_array(self, new_capacity: int):
+        return (new_capacity * ctypes.py_object)()
+
+    def _resize(self, new_capacity):
+        new_array = self._make_array(new_capacity)
+        for i in range(self._count):
+            new_array[i] = self.array[i]
+        self.array = new_array
+        self._capacity = new_capacity
+
+    def append(self, item: T):
+        if self._count == self._capacity:
+            self._resize(2 * self._capacity)
+        self.array[self._count] = item
+        self._count += 1
+
+    def insert(self, item: T, index: int) -> None:
+        if index < 0 or index > self._count:
+            self._insert_status = self.INSERT_ERR
+        else:
+            if index == self._count:
+                self.append(item)
+            if self._count == self._capacity:
+                self._resize(2 * self._capacity)
+
+            for j in range(self._count, index, -1):
+                self.array[j] = self.array[j - 1]
+            self.array[index] = item
+            self._count += 1
+            self._insert_status = self.INSERT_OK
+
+    def remove(self, index: int):
+        if index < 0 or index > self._count:
+            self._remove_status = self.REMOVE_ERR
+        else:
+            for j in range(index, self._count - 1):
+                self.array[j] = self.array[j + 1]
+            self._count -= 1
+            if self._capacity == self.default_capacity:
+                return
+            if self.filling_degree() < 0.5:
+                new_capacity = int(self._capacity / 1.5)
+                if new_capacity > self.default_capacity:
+                    self._resize(new_capacity)
+                else:
+                    self._resize(self.default_capacity)
+            self._remove_status = self.REMOVE_OK
+
+    def get(self, index: int) -> T:
+        if index < 0 or index >= self._count:
+            self._get_status = self.GET_ERR
+        else:
+            self._get_status = self.GET_OK
+            return self.array[index]
+
+    def capacity(self) -> int:
+        return self._capacity
+
+    def count(self) -> int:
+        return self._count
+
+    def remove_status(self) -> int:
+        return self._remove_status
+
+    def insert_status(self) -> int:
+        return self._insert_status
