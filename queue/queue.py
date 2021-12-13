abd68c0f34bf5d93274a1b229ab05b1ce0ab18d6
Alexander Bondarenko <sascha.bondarenko@gmail.com>
2021-12-13 21:22:54 +0100

    queue, adt and realization

---
+++ queue/queue.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+T = TypeVar('T')
+
+
+class AQueue(ABC, Generic[T]):
+    DEQUEUE_NIL = 0
+    DEQUEUE_OK = 1
+    DEQUEUE_ERR = 2
+
+    # коструктор: создана пустая очередь
+
+    # команды:
+
+    # постусловие: элемент добавлен в конец очереди
+    @abstractmethod
+    def enqueue(self, item: T) -> None:
+        pass
+
+    # предусловие: очередь не пуста
+    # постусловие: первый элемент удален из очерери
+    @abstractmethod
+    def dequeue(self):
+        pass
+
+    # запросы:
+    # колличество элементов в очереди
+    @abstractmethod
+    def size(self):
+        pass
+
+    # возврат удаленного элемента
+    @abstractmethod
+    def dequeued_item(self) -> T:
+        pass
+
+    @abstractmethod
+    def dequeue_status(self) -> int:
+        pass
+
+
+class Queue(AQueue):
+
+    def __init__(self):
+        self.arr = []
+        self._dequeued_item = None
+        self._dequeue_status = self.DEQUEUE_NIL
+
+    def enqueue(self, item: T) -> None:
+        self.arr.append(item)
+
+    def dequeue(self):
+        if self.size() == 0:
+            self._dequeue_status = self.DEQUEUE_ERR
+            self._dequeued_item = None
+        else:
+            self._dequeued_item = self.arr.pop(0)
+            self._dequeue_status = self.DEQUEUE_OK
+
+    def size(self):
+        return len(self.arr)
+
+    def dequeued_item(self) -> T:
+        return self._dequeued_item
+
+    def dequeue_status(self) -> int:
+        return self._dequeue_status
